97a4c52c5a76a8f8363bd2f501b97ed20dafdc11
YONK17544 <131665737+YONK17544@users.noreply.github.com>
2025-05-19 18:25:01 -0400

    Updating directory page with directory and category components

---
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -0,0 +1,16 @@
+import CategoryItem from "../category-item/category-item.component.jsx";
+import "./directory.styles.scss";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className = "directory-container">
+      {categories.map(({title, id, imageUrl}) => (
+        <CategoryItem key = {id} category = {{title, id, imageUrl}} />
+      ))}
+
+
+    </div>
+  )
+}
+
+export default Directory
